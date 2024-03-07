d51959369ee54f893d7e993d105205fb75017f92
mancy <m@m.m.com>
2024-03-07 07:11:38 +0300

    importent fix for serializer

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -2,9 +2,11 @@ from rest_framework import serializers
 from . import models
 
 class BookingSerializer(serializers.ModelSerializer):
-    model = models.Booking
-    fields = "__all__"
+    class Meta:
+        model = models.Booking
+        fields = "__all__"
 
 class MenuSerializer(serializers.ModelSerializer):
-    model = models.Menu
-    fields = "__all__"
+    class Meta:
+        model = models.Menu
+        fields = "__all__"
